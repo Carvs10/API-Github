8b6d4edb57c72aba5e81a37e26ec81231f4ad568
Carvs10 <joaovcl10@hotmail.com>
2023-04-24 19:02:32 -0300

    work in progress

--- src/pages/RepositoriesPage/Filter/index.jsx
+++ src/pages/RepositoriesPage/Filter/index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container, Selector, Cleaner } from "./style";
 
-function Filter() {
-  const langs = [
-    { name: "Javascript", count: 5, color: "#FCC419" },
-    { name: "Shell", count: 2, color: "#CED4DA" },
-    { name: "PHP", count: 1, color: "#25BEFF" },
-  ];
-
-  const selectors = langs.map(({ name, count, color }) => (
+function Filter({ languages }) {
+  const selectors = languages.map(({ name, count, color }) => (
     <Selector key={name.toLowerCase()} color={color}>
       <spam>{name}</spam>
       <spam>{count}</spam>
@@ -23,4 +18,13 @@ function Filter() {
   );
 }
 
+Filter.propTypes = {
+  languages: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+      color: PropTypes.string,
+    }).isRequired
+  ).isRequired,
+};
 export default Filter;
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -17,11 +17,64 @@ function RepositoriesPage() {
     blog: "https://devsamurai.com.br",
     location: "São José dos Campos - SP",
   };
+
+  const repositories = [
+    {
+      name: "Repo 1",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "JavaScript",
+    },
+    {
+      name: "Repo 2",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "JavaScript",
+    },
+    {
+      name: "Repo 3",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "PHP",
+    },
+    {
+      name: "Repo 4",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "Ruby",
+    },
+    {
+      name: "Repo 5",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "Java",
+    },
+    {
+      name: "Repo 6",
+      description: "Template básico para projetos ReactJS",
+      html_url: "https://devsamurai.com.br",
+      language: "TypeScript",
+    },
+  ];
+
+  const stats = repositories.map((repository) => repository.language);
+  // .reduce((data, lan))
+
+  console.log(stats);
+
+  // Calculo de filters
+  const languages = [
+    { name: "Javascript", count: 3, color: "#FCC419" },
+    { name: "Shell", count: 2, color: "#CED4DA" },
+    { name: "PHP", count: 2, color: "#25BEFF" },
+    { name: "Ruby", count: 1, color: "#e74c3c" },
+  ];
+
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter />
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories />
